34531bed931f125c5f5ae00e97275e80d198d259
Kim Ly Buck <kim.3993@gmail.com>
2020-06-14 18:53:44 -0500

    Post done

--- projectsRouter.js
+++ projectsRouter.js
@@ -4,30 +4,26 @@ const router = express.Router();
 
 const db = require('./data/helpers/projectModel');
 
+//CREATE
+router.post('/', validateProject, async (req, res) => {
+	const project = await db.insert(req.body);
+	res.status(201).json(project);
+});
+
 // READ
 router.get('/:id', loadProject, (req, res) => {
 	res.json(req.project);
 });
 
-
-// router.post('/', validateUser, async (req, res) => {
-//   const user = await db.insert(req.body);
-//   res.status(201).json(user);
-// });
-
 // router.post('/:id/posts', (req, res) => {
 // 	// do your magic!
 // });
-
-
 
 //passing middle ware to this handle only
 
 // router.get('/:id', validateUserId, (req, res) => {
 // 	res.json(req.user);
 // });
-
-
 
 // router.get('/:id/posts', validateUserId, (req, res) => {
 // 	const posts = userDb.getUserPosts(req.user.id);
@@ -61,19 +57,23 @@ async function loadProject(req, res, next) {
 	next();
 }
 
-//validateUser middleware
-// async function validateUser(req, res, next) {
-// 	if (!req.body) {
-// 		return res.status(400).json({
-// 			message: 'missing user data'
-// 		});
-// 	} else if (!req.body.name) {
-// 		return res.status(400).json({
-// 			message: 'missing required name field'
-// 		});
-// 	}
-// 	next();
-//}
+//validateProject middleware
+function validateProject(req, res, next) {
+	if (!req.body) {
+		return res.status(400).json({
+			message: 'missing project data'
+		});
+	} else if (!req.body.name){
+		return res.status(400).json({
+			message: 'missing required name field'
+		});
+	} else if (!req.body.description){
+        return res.status(400).json({
+			message: 'missing required description field'
+		});
+    }
+	next();
+}
 
 //validatePost middleware
 // async function validatePost(req, res, next) {
