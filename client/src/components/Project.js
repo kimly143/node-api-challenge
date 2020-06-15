1c2202fb7eb8cc4b2746f0c281a35f9fade12e2b
Kim Ly Buck <kim.3993@gmail.com>
2020-06-14 23:39:43 -0500

    added

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -4,5 +4,11 @@ import { useSelector } from 'react-redux';
 export default function Project() {
 	const project = useSelector((state) => state.projects.project);
 	if (!project) return null;
-	return <div>{project.name}</div>;
+    return (
+    <div>
+        <h3>{project.name}</h3>
+        <p>{project.description}</p>
+        <p>{project.notes}</p>
+        <button>Complete?</button>
+    </div>);
 }
